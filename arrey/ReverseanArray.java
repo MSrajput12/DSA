2896e013ec3afce7af3fd2fb1c79ac8cfd600787
MSrajput12 <manishrajput57427@gmail.com>
2026-03-06 20:20:31 +0530

    add arrey code

---
+++ arrey/ReverseanArray.java
@@ -0,0 +1,15 @@
+package arrey;
+
+public class ReverseanArray {
+    public static void main(int []arr){
+        int start =0;
+        int end=arr.length-1;
+        while(start<end){
+            int temp= arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
