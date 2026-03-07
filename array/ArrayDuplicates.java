bd8cddbec0f10ddcba2d33cdd3369544b09ca63c
MSrajput12 <manishrajput57427@gmail.com>
2026-03-07 20:54:48 +0530

    add arrey code

--- array/ArrayDuplicates.java
+++ array/ArrayDuplicates.java
@@ -5,21 +5,18 @@ import java.util.HashSet;
 
 public class ArrayDuplicates {
         public ArrayList<Integer> duplicates(int arr[]) {
-
-               HashSet<Integer> set = new HashSet<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        
-        for(int i=0;i<arr.length;i++){
+            HashSet<Integer> set = new HashSet<>();
+            ArrayList<Integer> result = new ArrayList<>();
             
-            if(set.contains(arr[i])){
-                result.add(arr[i]);
+            for(int i=0;i<arr.length;i++){
+                if(set.contains(arr[i])){
+                    result.add(arr[i]);
             }
             else{
                 set.add(arr[i]);
             }
         }
-        
         return result;
-        }
     }
+}
 
