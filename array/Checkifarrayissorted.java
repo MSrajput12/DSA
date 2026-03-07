a4aaa1a13c4235d12c01f56030791f5b9b6485e8
MSrajput12 <manishrajput57427@gmail.com>
2026-03-07 20:32:23 +0530

    add arrey code

---
+++ array/Checkifarrayissorted.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Checkifarrayissorted {
+    public boolean arraySortedOrNot (int[] arr){
+        for( int i =0;i<arr.length-1;i++){
+            if (arr[i]>arr[i+1]){
+                return false;
+
+            }
+            return true;
+        }
+    }
+
+    
+}
